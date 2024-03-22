4ce36aa37fccca035863a1cc37746278c72fd444
Scott Gilmore <scott@gliant.com>
2024-03-21 18:48:16 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <ChatView msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatView from './components/ChatView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChatView
   }
 }
 </script>
@@ -23,4 +23,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style>./components/ChatView.vue
---
+++ src/components/ChatView.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <deep-chat
+    :demo="true"
+    :textInput="{ placeholder: { text: 'Type here...' } }"
+    :initialMessages="initialMessages"
+  />
+</template>
+
+<script>
+import "deep-chat";
+
+export default {
+  name: "ChatView",
+  data() {
+    return {
+      initialMessages: [
+        { role: "ai", text: "Good morning! Can I help you with a clinical question?" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+div {
+  font-family: sans-serif;
+  text-align: center;
+  justify-content: center;
+  display: grid;
+}
+</style>
